eca44c4727e469f1fae239279e083bdf9d9d41bc
erickcartiady <erickcartiady@gmail.com>
2020-05-29 22:17:33 +0700

    Updates

---
+++ precache-manifest.c585d5facdccada02e95e217ea695ce6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f53314b2525b30563436840028e079",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "56220435d5a2905c077a",
+    "url": "/movie-app/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "c348a7384f725f4b82ab",
+    "url": "/movie-app/static/css/main.e5361b36.chunk.css"
+  },
+  {
+    "revision": "56220435d5a2905c077a",
+    "url": "/movie-app/static/js/2.563e0d0a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/movie-app/static/js/2.563e0d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c348a7384f725f4b82ab",
+    "url": "/movie-app/static/js/main.89429b3a.chunk.js"
+  },
+  {
+    "revision": "45768a3bf89ec2ac09c8",
+    "url": "/movie-app/static/js/runtime-main.f578d72d.js"
+  }
+]);
